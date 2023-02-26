cdb53b0eb3d20f01048af8e3ab50c4d2c0480a17
Sean Matheis <97707408+seanmatheis@users.noreply.github.com>
2023-02-26 17:11:26 -0500

    Adding Assignment 9 Hello World Files

---
+++ OneBlink.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(8, OUTPUT);    // sets the digital pin 13 as output
+  pinMode(5, INPUT);
+}
+
+void loop() {
+
+  if(digitalRead(5) == HIGH)
+    digitalWrite(8, HIGH); // sets the digital pin 13 on
+    delay(1000);            // waits for a second
+    digitalWrite(8, LOW);  // sets the digital pin 13 off
+    delay(1000);            // waits for a second
+}
